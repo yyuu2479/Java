119a3e0922346eabcf83e0c7709a07847563ea56
yyuu2479 <yyuu2479@gmail.com>
2021-11-02 12:54:55 +0900

    java_variable

---
+++ Ready02/Ready02.java
@@ -0,0 +1,8 @@
+// クラスブロックの始まり
+public class Ready02 {
+  // メゾットブロックの始まり
+  public static void main(String[] args) {
+    // メゾット処理記述
+    System.out.println("Hello World!!");
+  }
+}
---
+++ Ready03/Chapter03.java
@@ -0,0 +1,10 @@
+public class Chapter03 {
+  public static void main(String[] args) {
+    String str1 = "山内佑泰";
+    System.out.println(str1);
+    str1 = "お金";
+    System.out.println(str1);
+    final int LUCKY_NUMBER = 2;
+    System.out.println(LUCKY_NUMBER);
+  }
+}
