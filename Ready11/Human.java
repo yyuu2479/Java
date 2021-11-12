980b513c31d3bcec5e1a1e49bf0748482397881e
yyuu2479 <yyuu2479@gmail.com>
2021-11-12 15:11:58 +0900

    java_inheritance

---
+++ Ready11/Human.java
@@ -0,0 +1,17 @@
+public class Human {
+  private String name;
+  protected  int age;
+
+  public Human(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public int getAge() {
+    return this.age;
+  }
+}
