9bffec3b02ddf11341bd2499ef7cca22e59a56e2
yyuu2479 <yyuu2479@gmail.com>
2021-12-06 23:51:47 +0900

    java

--- Ready07/For02.java
+++ Ready07/For02.java
@@ -2,7 +2,7 @@ public class For02 {
   public static void main(String[] args) {
     int[] array = { 1, 2, 3, 4, 5 };
     for (int number : array) {
-      System.out.println("For2 = " + number);
+      System.out.println("出力結果 = " + number);
     }
   }
 }
